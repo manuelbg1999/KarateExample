5452ca70aa773983ba755efc41d6fa1f66c8c8fe
manuelbg1999 <manuelbg1999@gmail.com>
2023-08-22 10:01:07 -0500

    commit inicial

--- src/test/java/user/runner.java
+++ src/test/java/user/runner.java
@@ -2,7 +2,8 @@ package user;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+///import org.junit.jupiter.api.Test;
 
 public class runner {
 //Desde esta clase ejecutamos todos los features que tenemos
